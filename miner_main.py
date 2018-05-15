2505959ebd157c3543aee018453994bb64aed46d
Viapunk <pwilk.uk@gmail.com>
2018-05-15 22:42:16 +0200

    Added argument parser, readme, requirements, and more things do add later.

--- miner_main.py
+++ miner_main.py
@@ -4,58 +4,88 @@ import paramiko
 import time
 from paramiko import SSHException
 import argparse
+from getpass import getpass
 
-argument_parser = argparse.ArgumentParser(description="PJWSTK data searcher.")
-argument_parser.add_argument("-d", "--download", action="append", nargs=1)
-argument_parser.add_argument("-s", "--search", action="append", nargs="*")
-argument_parser.add_argument("-o", "--output", nargs=1)
-argument_parser.add_argument("")
+argparser = argparse.ArgumentParser(description="PJWSTK data searcher.")
 
-key_path = "C:/Users/Paweł/.ssh/id_rsa"
-index_file = "C:/Users/Paweł/Documents/workspace/indeksy.txt"
-key = paramiko.RSAKey.from_private_key_file(key_path)
-result_List = []
+argparser.add_argument("mode", choices=['search', 'download'],
+                       help='Program purpose.', default='search')
+
+argparser.add_argument("patterns", nargs="*",
+                       help="Patterns to search or file path to download.")
+
+argparser.add_argument("username", help="University login.", type=str)
+
+argparser.add_argument("--index_list", type=str, help="Path to file"
+                       "with students' index numbers", default=os.path.join(
+                           os.getcwd(), "indexes.txt"))
+
+argparser.add_argument("-t", "--timeout", type=int, help="Timeout between "
+                                                         "each ssh command",
+                       default=2)
+
+argparser.add_argument("-k", "--key", type=str,
+                       help="Path to private ssh key file.")
+
+argparser.add_argument("-o", "--output", type=str, help="Path to output file, "
+                       "only in download mode.",  default=os.path.join(
+                           os.getcwd(), "output.txt"))
+
+argparser.add_argument("-v", "--verbose", action="store_true",
+                       help="Verbose mode.")
+
+settings = argparser.parse_args()
+
+if settings.key:
+    rsakey = paramiko.RSAKey.from_private_key_file(settings.key)
+result_list = []
 index_list = []
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+with open(str(settings.index_list), 'r') as f:
+	index_list = [line.replace("\n", "") for line in f]
+	for index in index_list:
+		if index[0] is not 's':
+			index = 's' + index
+	print("Data prepared, attempting to establish a secure connection.")
 
-with open(index_file, 'r') as f:
-    index_list = [line.replace("\n", "") for line in f]
-    print("Data prepared, attempting to establish a secure connection.")
-    try:
-        client.connect("szuflandia.pjwstk.edu.pl", username="s13705", pkey=key)
-    except SSHException as e:
-        print("Connection failed, error:\n {}".format(e))
-    else:
-        print("Connection successfull, let's roll.")
-    try:
-        for index in index_list:
-            print("Mining data from {} student.".format(index))
-            stdin, stdout, stderr = client.exec_command("find /home/PJWSTK/{} -type f -name \"{}\"".format(index, "TIN"))
-            stdin.close()
-            stdout._set_mode('b')
-            time.sleep(2)
-            for line in stdout.readlines():
-                result_List.append(line)
-    except:
-        print("An Error ocurred, closing secure connection!")
-    finally:
-        print("Searching finished, closing secure connection.")
-        client.close()
+try:
+	if settings.key:
+		client.connect("szuflandia.pjwstk.edu.pl", username=settings.username,
+                       pkey=rsakey or None)
+	else:
+		client.connect("szuflandia.pjwstk.edu.pl", username=settings.username,
+                        password=getpass(prompt="Password: "))
+except SSHException as e:
+	print("Connection failed, error:\n {}".format(e))
+else:
+	print("Connection successfull, let's roll.")
 
-os.chdir("C:/Users/Paweł/Documents/workspace")
-with open("results.txt", "ab") as f:
-    for line in result_List:
+
+for pattern in settings.patterns:
+	print("Searching for {} pattern.".format(pattern))
+	for index in index_list[:20]:
+		print("Mining data from {} student.".format(index))
+		try:
+			stdin, stdout, stderr = client.exec_command(
+				"find /home/PJWSTK/{} -type f -name \"{}\"".format(index,
+				                                                   pattern))
+			time.sleep(settings.timeout)
+		except SSHException as e:
+			print("An error ocurred when trying to mine data: {}".format(e))
+		stdout._set_mode('b')
+		for line in stdout.readlines():
+			result_list.append(line)
+
+with open(settings.output, "ab") as f:
+    for line in result_list:
         f.write(line)
-        f.write(bytes("deadbeef", encoding='utf-8'))
 
-fdata = None
-with open("results.txt", "r") as f:
-    fdata = f.read()
+#with open(settings.output, "r") as f:
+#    fdata = f.read()
+#    fdata.replace("deadbeef", "\n")
 
-fdata.replace("deadbeef", "\n")
-
-with open("results.txt", "w") as f:
-    f.write(fdata)
+#with open(settings.output, "w") as f:
+#    f.write(fdata)
---
+++ readme.md
@@ -0,0 +1,17 @@
+###What you need to know
+* You need to be a student of PJATK or just have a working set of credentials to access the server(you may also use a private RSA key)
+* have a list of student indexes, separated by newline character.
+* install requirements from the file provided.
+
+
+#Usage:
+* I already do a lot for you, lazy person, find out for yourself how it works!
+* Hint: You may also try to execute it with -h option.
+
+
+##To do:
+* add silent mode
+* add logging and more logging options
+* add downloading option
+* make it less spaghetti code
+* add searching without students' index file
